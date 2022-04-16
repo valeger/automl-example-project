dbd88dd87f1f9ca8cad0d4a6382b90875e853727
valeger <43153180+valeger@users.noreply.github.com>
2022-04-16 18:45:56 +0300

    Update model_1.py

--- models/model_1.py
+++ models/model_1.py
@@ -1,4 +1,11 @@
+import os
 from time import sleep
 
-sleep(1)
-print("Model_1 is trained successfully")
+if (
+  not os.getenv("AWS_ACCESS_KEY_ID")
+  and not os.getenv("AWS_SECRET_ACCESS_KEY")
+):
+  raise RuntimeError("Envs variables were not found")
+else:
+  sleep(1)
+  print("Model_1 is trained successfully")
